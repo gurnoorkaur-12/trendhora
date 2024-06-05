a4b2671829d4f0ac0579c9ce9d848ef28368ca84
agamjotsingh18 <70067726+agamjotsingh18@users.noreply.github.com>
2024-06-05 22:51:53 +0530

    Update db.js

--- server/config/db.js
+++ server/config/db.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const connectDB = async () => {
     try {
         mongoose.set('strictQuery', false); // Add this line
-        const conn = await mongoose.connect(process.env.MONGO_URI);
+        const conn = await mongoose.connect(process.env.MONGO_URI + "/Trendhora");
         console.log("Mongo DB Connected: ", conn.connection.host);
     } catch (err) {
         console.log(err);
