04bb23f9eac0122a79bd3dd9d9d59f746c753b7d
agamjotsingh18 <70067726+agamjotsingh18@users.noreply.github.com>
2024-07-22 05:25:42 +0530

    Update ItemCard.js

--- client/src/components/Card/ItemCard/ItemCard.js
+++ client/src/components/Card/ItemCard/ItemCard.js
@@ -8,24 +8,26 @@ import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
 import { WishItemsContext } from '../../../Context/WishItemsContext';
 
 const ItemCard = (props) => {
-    const [isHovered, setIsHovered] = useState(false)
-    const  cartItemsContext  = useContext(CartItemsContext)
-    const wishItemsContext = useContext(WishItemsContext)
+    const [isHovered, setIsHovered] = useState(false);
+    const cartItemsContext = useContext(CartItemsContext);
+    const wishItemsContext = useContext(WishItemsContext);
 
     const handleAddToWishList = () => {
-        wishItemsContext.addItem(props.item)
-    }
+        wishItemsContext.addItem(props.item);
+    };
 
     const handleAddToCart = () => {
-        cartItemsContext.addItem(props.item, 1)
-    }
+        cartItemsContext.addItem(props.item, 1);
+    };
 
     if (!props.item || !props.item.category || !props.item.image || props.item.image.length === 0) {
-        return null; // Avoid rendering if item or required properties are not defined
+        return null; // Avoid rendering if item is not defined or missing required properties
     }
 
     const getImageUrl = (image) => {
-        return `https://trendhora-api.onrender.com/public/${props.item.category}/${image.filename}`;
+        const url = `https://trendhora-api.onrender.com/public/${props.item.category}/${image.filename}`;
+        console.log('Image URL:', url);
+        return url;
     };
 
     return (
@@ -43,7 +45,7 @@ const ItemCard = (props) => {
                 <div className="product__card__detail">
                     <div className="product__name">
                         <Link to={`/item/${props.item.category}/${props.item._id}`}>
-                           {props.item.name}
+                            {props.item.name}
                         </Link>
                     </div>
                     <div className="product__description">
@@ -53,17 +55,17 @@ const ItemCard = (props) => {
                         <span>${props.item.price}</span>
                     </div>
                     <div className="product__card__action">
-                        <IconButton onClick={handleAddToWishList} sx={ {borderRadius: '20px', width: '40px', height: '40px', /* borderWidth: '3px', borderStyle: 'solid', borderColor: '#FFE26E' */ }  }>
-                            <FavoriteBorderIcon sx={{width: '22px', height: '22px', color: 'black'}}/>
+                        <IconButton onClick={handleAddToWishList} sx={{ borderRadius: '20px', width: '40px', height: '40px' }}>
+                            <FavoriteBorderIcon sx={{ width: '22px', height: '22px', color: 'black' }} />
                         </IconButton>
-                        <IconButton onClick={handleAddToCart} sx={ {borderRadius: '20px', width: '40px', height: '40px' /*  borderWidth: '3px', borderStyle: 'solid', borderColor: '#FFE26E' */}}>
-                            <AddShoppingCartIcon sx={{width: '22px', height: '22px', color: 'black'}}/>
-                        </IconButton >
+                        <IconButton onClick={handleAddToCart} sx={{ borderRadius: '20px', width: '40px', height: '40px' }}>
+                            <AddShoppingCartIcon sx={{ width: '22px', height: '22px', color: 'black' }} />
+                        </IconButton>
                     </div>
                 </div>
             </div>
         </div>
-     );
-}
- 
+    );
+};
+
 export default ItemCard;
