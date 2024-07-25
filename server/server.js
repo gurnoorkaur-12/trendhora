fb84bcc8d0629758212c77e5f7f56742a046096a
agamjotsingh18 <70067726+agamjotsingh18@users.noreply.github.com>
2024-07-25 21:30:56 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -11,7 +11,6 @@ connectDB();
 // Middlewares
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(express.static("public"));
 app.use(cors());
 
 app.use('/public', express.static(path.join(__dirname, 'public')));
