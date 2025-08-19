62c2857edc65fcec4bf33dab7e94dcbcd0c1e514
Jaya Jha <jayajha122006@gmail.com>
2025-08-19 19:53:40 +0530

    updated npm command version

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -3,13 +3,6 @@ const jwt = require('jsonwebtoken');
 require('dotenv').config();
 const {asyncHandler }= require('../utility/asyncHandler');
 
-exports.registerUser = async (req, res) => {
-  const { username, email, password } = req.body;
-  try {
-    const userExists = await User.findOne({ email });
-    if (userExists) {
-      return res.status(400).json({ message: 'User already exists' });
-    }
 exports.registerUser = asyncHandler(async (req, res) => {
     const { username, email, password } = req.body;
 
@@ -25,14 +18,6 @@ exports.registerUser = asyncHandler(async (req, res) => {
     const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, { expiresIn: '1h' });
 
     res.status(201).json({ token });
-  } catch (error) {
-    res.status(500).json({ message: error.message });
-  }
-};
-
-    const user = new User({ username, email, password });
-    await user.save();
-    res.status(201).send('User registered successfully');
 });
 
 exports.loginUser = asyncHandler(async (req, res) => {
